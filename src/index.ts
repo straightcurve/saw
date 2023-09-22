f7ed84cc563646a4bc289133c1624c7c2f8215cb
A2 <antoniu.ilea@gmail.com>
2023-09-22 10:09:33 +0300

    fix(format): use spaces

--- src/index.ts
+++ src/index.ts
@@ -5,18 +5,18 @@ type FnLike<T> = () => T | Promise<T>;
  * @description wraps a sync/async function call in a try/catch
  */
 const saw = async <T>(fn: FnLike<T>): Promise<[unknown, null] | [null, T]> => {
-	try {
-		const result = fn();
+    try {
+        const result = fn();
 
-		//  @note: awaiting sync functions has non-trivial overhead
-		if (result instanceof Promise) {
-			return [null, await result];
-		}
+        //  @note: awaiting sync functions has non-trivial overhead
+        if (result instanceof Promise) {
+            return [null, await result];
+        }
 
-		return [null, result];
-	} catch (error) {
-		return [error, null];
-	}
+        return [null, result];
+    } catch (error) {
+        return [error, null];
+    }
 };
 
 export default saw;
