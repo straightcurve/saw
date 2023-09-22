d7324af0278fecdada695c11da81323ea3060911
A2 <antoniu.ilea@gmail.com>
2023-09-22 09:24:37 +0300

    init()

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+type FnLike<T> = () => T | Promise<T>;
+
+/**
+ * @summary sync/async wrap
+ * @description wraps a sync/async function call in a try/catch
+ */
+const saw = async <T>(fn: FnLike<T>): Promise<[unknown, null] | [null, T]> => {
+	try {
+		const result = fn();
+
+		//  @note: awaiting sync functions has non-trivial overhead
+		if (result instanceof Promise) {
+			return [null, await result];
+		}
+
+		return [null, result];
+	} catch (error) {
+		return [error, null];
+	}
+};
+
+export default saw;
